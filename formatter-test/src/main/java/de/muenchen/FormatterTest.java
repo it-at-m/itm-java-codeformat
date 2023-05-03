65e340c8c4b30ee14723e529e3e9f998affe75e6
Mirna Rodic <mirna.rodic10@gmail.com>
2023-05-03 16:22:55 +0200

    :tada: Upload source code

---
+++ formatter-test/src/main/java/de/muenchen/FormatterTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2022
+ */
+package de.muenchen;
+
+/**
+ * @author michael.prankl
+ *
+ */
+public class FormatterTest {
+
+    public static final String LONG_LINE = "this is a very very very very very very very very very very veryvery very very very veryvery very very very veryvery very very very veryvery very very very veryvery very very very very long string without breaks";
+
+    /**
+     * a long long long long long long long long long long long long long long long long long long
+     * long long long long long long long long long long long long long longlong long long long
+     * javadoc comment
+     * 
+     * @param param1
+     * @param param2
+     * @param param3
+     * @param param4
+     * @param parameterNameWithLongName
+     * @param muchMore
+     */
+    public void aMethod(String param1, String param2, String param3, String param4, String parameterNameWithLongName,
+            String muchMore) {
+        // a long long long long long long long long long long long long long long long long long long long long long long long long  long long long long long long long longlong long long long comment
+    }
+}
