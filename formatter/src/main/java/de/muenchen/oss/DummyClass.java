f1b17673e298bb523d133e1d9257183e48e92854
eidottermihi <eidottermihi@gmail.com>
2024-05-27 10:38:44 +0200

    provide some dummy java to fulfill javadoc requirements of Sonatype

---
+++ formatter/src/main/java/de/muenchen/oss/DummyClass.java
@@ -0,0 +1,9 @@
+package de.muenchen.oss;
+
+/**
+ * This is just a dummy class to fulfill Sonatypes need to have javadoc for
+ * every artifact.
+ */
+public class DummyClass {
+
+}
---
+++ formatter/src/main/java/de/muenchen/oss/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This is just a dummy javadoc to fulfill Sonatypes need to have javadoc for every artifact.
+ */
+package de.muenchen.oss;
